6b474478914d01571e6beeeb555f80a268987f76
tristann3 <tristan15thompson@gmail.com>
2022-05-11 15:47:53 -0400

    can print search term to console using os lib

--- search.go
+++ search.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/rocketlaunchr/google-search"
+	"os"
 )
 
 func main() {
 	fmt.Println(googlesearch.Search(nil, "cars for sale in Toronto, Canada"))
+
+	term := os.Args[1]
+	fmt.Println(term)
+
 }
