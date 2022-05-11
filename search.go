1e5729feb71634a3ea878070bd60ad95cc103315
tristann3 <tristan15thompson@gmail.com>
2022-05-11 16:17:25 -0400

    search results are now stored in an array of search result custom types

--- search.go
+++ search.go
@@ -6,10 +6,31 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Println(googlesearch.Search(nil, "cars for sale in Toronto, Canada"))
+type SearchResult struct {
+	Title string
+	URL string
+}
 
+func buildResults(data []googlesearch.Result) []SearchResult{
+	numResults := len(data)
+	var results []SearchResult
+	for i := 0; i < numResults; i++ {
+		results = append(results, SearchResult{
+			data[i].Title,
+			data[i].URL})
+	}
+	return results
+}
+
+func main() {
+	// Here we get the google search term from the console arguments
 	term := os.Args[1]
 	fmt.Println(term)
+
+	data, _ := googlesearch.Search(nil, term)
+
+	results := buildResults(data)
+	fmt.Println(results[1].URL)
+
 
 }
